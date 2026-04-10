510cb9d6477d6e1df48210656f863193598015f7
mynul56 <mynulislamtanim@gmail.com>
2026-04-10 16:26:00 +0600

    fix: update SDK versions and deployment target for iOS and Android

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.skinseek.skinseek_app"
-    compileSdk = 36
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,10 +24,11 @@ android {
         applicationId = "com.skinseek.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        minSdk = 21
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     flavorDimensions += "version"
